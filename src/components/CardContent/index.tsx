848f225ea9a4c8b86d073ee043691a4a8608f3f2
Gabriel Matos Lourenço <matos.gabriel2013@gmail.com>
2023-04-16 17:47:28 -0300

    feat(cardcontent): adding the necessary properties defined by the Card Component interface

--- src/components/CardContent/index.tsx
+++ src/components/CardContent/index.tsx
@@ -1,12 +1,30 @@
 import { Card } from "../Card";
 import { CardContentContainer } from "./styles";
 
-export function CardContent(){
-    return(
-        <CardContentContainer>
-            <Card />
-            <Card />
-            <Card />
-        </CardContentContainer>
-    );
+export function CardContent() {
+  return (
+    <CardContentContainer>
+      <Card
+        benefits="Autoatendimento"
+        buttonContent="Assinar agora"
+        price={19.90}
+        title="Essentials"
+        subtitle="Para você começar"
+      />
+      <Card
+        benefits="Autoatendimento"
+        buttonContent="Assinar agora"
+        price={21.25}
+        title="Essentials"
+        subtitle="Para você começar"
+      />
+      <Card
+        benefits="Autoatendimento"
+        buttonContent="Assinar agora"
+        title="Essentials"
+        subtitle="Para você começar"
+        price={21.25}
+      />
+    </CardContentContainer>
+  );
 }

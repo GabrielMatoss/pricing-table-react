ab3702ebed69ab129d17fdcc38278a02bd82bec8
Gabriel Matos Lourenço <matos.gabriel2013@gmail.com>
2023-04-17 10:39:53 -0300

    feat(utils): add creation of the CardItems file that contains an array with card data

---
+++ src/utils/CardItens.ts
@@ -0,0 +1,40 @@
+
+
+export interface CardProps{
+    id: number,
+    buttonContent: string;
+    title: string;
+    subtitle: string;
+    price?: number[];
+    benefits: string[];
+    icon?: boolean;
+}
+
+export const cardItens:CardProps[] = [
+    {
+        id: 0,
+        title: "Essentials",
+        subtitle: "Para você começar",
+        buttonContent: "Assinar agora",
+        price: [20.02],
+        benefits: ["Até 3 usuários", "Autoatendimento"],
+        icon: true,
+    },
+    {   
+        id: 1,
+        title: "Ultimate",
+        subtitle: "Para você decolar",
+        buttonContent: "Assinar agora",
+        price: [29.97],
+        benefits: ["Usuários ilimitados", "Suporte 24/7", "CSM Dedicado", "Treinamentos"],
+        icon: true,
+    },
+    {   
+        id: 2,
+        title: "Enterprise",
+        subtitle: "Para sua empresa",
+        buttonContent: "Fale com a gente",
+        benefits: ["Plano customizado especialmente para a necessidade de seu negocio"],
+        icon: false,
+    }
+]

93e44b363258ace401e341c5c66829006773c90f
Gabriel Matos Lourenço <matos.gabriel2013@gmail.com>
2023-04-17 10:40:56 -0300

    feat(cardcontent): add key property in Card component mapping

--- src/components/CardContent/index.tsx
+++ src/components/CardContent/index.tsx
@@ -1,30 +1,13 @@
+import { cardItens } from "../../utils/CardItens";
 import { Card } from "../Card";
 import { CardContentContainer } from "./styles";
 
 export function CardContent() {
   return (
     <CardContentContainer>
-      <Card
-        benefits="Autoatendimento"
-        buttonContent="Assinar agora"
-        price={19.90}
-        title="Essentials"
-        subtitle="Para você começar"
-      />
-      <Card
-        benefits="Autoatendimento"
-        buttonContent="Assinar agora"
-        price={21.25}
-        title="Essentials"
-        subtitle="Para você começar"
-      />
-      <Card
-        benefits="Autoatendimento"
-        buttonContent="Assinar agora"
-        title="Essentials"
-        subtitle="Para você começar"
-        price={21.25}
-      />
+      {cardItens.map((items) => (
+        <Card key={items.id} items={items} />
+      ))}
     </CardContentContainer>
   );
 }
